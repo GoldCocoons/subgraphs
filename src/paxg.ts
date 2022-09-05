fca3ddbca4771e959fb001a57b036a6042e6efdc
bigchiano <bigchiano@protonmail.com>
2022-09-05 12:43:40 +0300

    add todo comments

--- src/paxg.ts
+++ src/paxg.ts
@@ -2,4 +2,9 @@ import {
   Transfer
 } from "../generated/paxg/paxg";
 
-export function handleTransfer(event: Transfer): void { }
+export function handleTransfer(event: Transfer): void {
+  // TODO:
+  //1. create Transfer entity
+  //2. create Address entity with the sender and receiver address
+  //3. create Balance entity with the sender and receiver address
+}
